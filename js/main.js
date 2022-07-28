9ab0864d690f0c1bfe70fe6aa8b85aad3063f607
jrlopez185 <79105416+jrlopez185@users.noreply.github.com>
2022-07-27 19:02:47 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let root = document.getElementById("root");
+
+let contenedor = '<img class="imagen" src="https://github.com/jrlopez185/imagenes-suvir/blob/main/Aliba.png?raw=true">'+'<div class="contenedor">'+ '<div class="respuesta">'+'</div>'+
+'<div class="menu">'+'<div class="uno">'+'<h1 class="h_Uno">'+'carts_has_products'+'</h1>'+'</div>'+'<div class="dos">'+'<h1 class="h_dos">'+'category'+'</h1>'+'</div>'+'<div class="tres">'+'<h1 class="h_tres">'+'option'+'</h1>'+'</div>'+'<div class="cuatro">'+'<h1 class="h_cuatro">'+'order'+'</h1>'+'</div>'
++'<div class="cinco">'+'<h1 class="h_cinco">'+'orders_has_products'+'</h1>'+'</div>'+ '<div class="seis">'+'<h1 class="h_seis">'+'orders_paid_creditcard'+'</h1>'+'</div>'+'<div class="siete">'+'<h1 class="h_siete">'+'orders_placed_user'+'</h1>'+'</div>'+'<div class="ocho">'+'<h1 class="h_ocho">'+'product_sold_vendor'+'</h1>'+'</div>'+'<div class="nueve">'+'<h1 class="h_nueve">'+'products'+'</h1>'+'</div>'
++'<div class="dies">'+'<h1 class="h_dies">'+'products_belong_category'+'</h1>'+'</div>'+ '<div class="once">'+'<h1 class="h_once">'+'products_has_options'+'</h1>'+'</div>'+'<div class="doce">'+'<h1 class="h_doce">'+'shoppingcart'+'</h1>'+'</div>'+'<div class="trece">'+'<h1 class="h_trece">'+'user'+'</h1>'+'</div>'+'<div class="catorce">'+'<h1 class="h_catorce">'+'user_has_creditcard'+'</h1>'+'</div>'
++'<div class="quince">'+'<h1 class="h_quince">'+'vendor'+'</h1>'+'</div>'
+
+
+
+
+
+
+
+
+
+root.innerHTML = contenedor
---
+++ js/shoppingcart.js
@@ -0,0 +1,5 @@
+let shoppingcart = [
+    [3000,3100,3200,3300,3400,3500,3600],
+    [1,1,0,0,1,0,1],
+]
+console.log(shoppingcart[0]);
